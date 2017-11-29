ccc7003fa31545f1e880b28b0acffcd766a8e4d2
Álvaro Santisteban <alvarosantis97@gmail.com>
2017-11-29 14:19:53 +0100

    Update

--- Assets/Scripts/enemyDamage.cs
+++ Assets/Scripts/enemyDamage.cs
@@ -5,17 +5,62 @@ using UnityEngine;
 public class enemyDamage : MonoBehaviour
 {
     public float damage;
+    public float damageRate;
     public float pushBackForce;
 
     float nextDamage;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool playerInRange = false;
+
+    GameObject player;
+    PlayerHealth playerHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        nextDamage = Time.time;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange) Attack();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Attack()
+    {
+        if (nextDamage <= Time.time)
+        {
+            playerHealth.hitDamage(damage);
+            nextDamage = Time.time + damageRate;
+
+            pushBack(player.transform);
+        }
+    }
+
+    void pushBack (Transform pushedObject)
+    {
+
+    }
+
+    
 }
