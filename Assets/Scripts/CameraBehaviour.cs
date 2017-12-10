39398370c37a49bbad66658622cdfce22e938c4f
Álvaro Santisteban <alvarosantis97@gmail.com>
2017-12-10 21:48:44 +0100

    Update

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraBehaviour : MonoBehaviour {
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Vector2 offset;
+    public float smoothTime;
+    private Vector3 currentVelocity;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public PlayerBehaviour player;
+
+    void FixedUpdate()
+    {
+        if (player.facingRight) offset.x = Mathf.Abs(offset.x);
+        else offset.x = -Mathf.Abs(offset.x);
+
+        Vector3 newPosition = new Vector3(targetTransform.position.x + offset.x, targetTransform.position.y + offset.y, targetTransform.position.z + transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref currentVelocity, smoothTime);
+    }
 }
