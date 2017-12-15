195ab3d2e97db86b0e2a2261cfbb37f799ed6122
Álvaro Santisteban <alvarosantis97@gmail.com>
2017-12-15 13:46:18 +0100

    logoscreen and fading

---
+++ Assets/Scripts/SceneTransitionCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneTransitionCounter : MonoBehaviour
+{
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("menuScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            SceneManager.LoadScene("menuScene");
+    }
+}
