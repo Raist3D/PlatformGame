59d5368b02a376215a9f5b1a4bd9717ef17aa575
Álvaro Santisteban <alvarosantis97@gmail.com>
2017-12-15 01:04:20 +0100

    main menu & options implemented + pause game

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+
+    bool pause;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButtonDown("Cancel")) pause = !pause; 
+
+        if(pause)  Time.timeScale = 0; 
+
+        if(!pause)  Time.timeScale = 1; 
+    }
+}
