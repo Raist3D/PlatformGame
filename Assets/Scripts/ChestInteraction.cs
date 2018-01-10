9f3a4763051673eaf9cfbaf658fb47ef49940e25
- <->
2018-01-10 19:15:29 +0100

    Menu and HUD Update

---
+++ Assets/Scripts/ChestInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestInteraction : MonoBehaviour
+{
+
+    public bool drop;
+    public GameObject dropItem;
+
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && Input.GetButtonDown("Interact"))
+        {
+            Destroy(this.gameObject);
+            if(drop) Instantiate(dropItem, transform.position, transform.rotation);
+            anim.SetTrigger("openChest");
+        }
+
+    }
+}
