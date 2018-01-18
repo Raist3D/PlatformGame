7d34ed2ecb55f401a7a54e18b2510c536d1876c5
Álvaro Santisteban <->
2018-01-18 19:06:51 +0100

    Enemy popUpTextDMG Update

---
+++ Assets/Scripts/UI/FloatingText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour
+{
+    public Animator anim;
+    private Text damageText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        AnimatorClipInfo[] clipInfo = anim.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+        damageText = anim.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void SetText(string text)
+    {
+        damageText.text = text;
+    }
+}
---
+++ Assets/Scripts/UI/FloatingTextController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextController : MonoBehaviour
+{
+    private static FloatingText popUpText;
+    private static GameObject canvas;
+
+
+    public static void Initialize()
+    {
+        canvas = GameObject.Find("Canvas");
+        popUpText = Resources.Load<FloatingText>("Prefabs/PopUpTextParent");
+    }
+
+    public static void CreateFloatingText(string text, Transform location)
+    {
+        FloatingText instance = Instantiate(popUpText);
+        instance.transform.SetParent(canvas.transform, false);
+        instance.SetText(text);
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
