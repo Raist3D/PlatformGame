beb58e37d63442c4413c38f0a2367f57438fa007
Álvaro Santisteban <alvarosantis97@gmail.com>
2018-02-09 12:43:48 +0100

    Background music provisional & tutorial popUP messages

---
+++ Assets/Scripts/TutorialMesages.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TutorialMesages : MonoBehaviour
+{
+
+    public GameObject checker;
+    public bool playerNear;
+    public Text tutorialMessage;
+
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerNear = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(checker.GetComponent<CercanyChecker>().detected)
+        {
+            tutorialMessage.gameObject.SetActive(true);
+
+        }
+        else
+        {
+            tutorialMessage.gameObject.SetActive(false);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerNear = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerNear = false;
+        }
+    }
+}
